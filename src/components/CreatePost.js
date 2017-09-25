bbda973bed72b894d59a4dddc32c845a65dca9b1
Sarah Zajac <sarahmzajac@gmail.com>
2017-09-25 15:19:58 -0400

    can now post

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -10,6 +10,40 @@ export default class CreatePost extends Component{
     post: "",
   }
 
+  handleName = (e) => {
+    this.setState({authorName: e.target.value});
+  }
+
+  handleTitle = (e) => {
+    this.setState({blogTitle: e.target.value});
+  }
+
+  handlePost = (e) => {
+    this.setState({post: e.target.value});
+  }
+
+  addToBlog = (e) =>{
+    e.preventDefault();
+    this.setState({authorName: e.target.value, blogTitle: e.target.value, post: e.target.value});
+    let listItem = JSON.stringify(this.state);
+
+    fetch('https://tiny-lasagna-server.herokuapp.com/collections/blogger/',{
+      method: "POST",
+      body: listItem,
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+  ).then(response => {
+        console.log(response, "yay");
+
+      }).catch(err => {
+        console.log(err, "boo!");
+      });
+      this.setState({authorName: '', blogTitle: '', post: ''});
+  }
+
   render(){
     return(
       <div>
